a93f415153fe5304043404080510aa3e8ceaefa5
Lars <larsriel@gmail.com>
2021-11-24 11:52:06 +0100

    first commit

---
+++ src/main/java/com/fourmuchachos/gifmycrypto/GifmycryptoApplication.java
@@ -0,0 +1,13 @@
+package com.fourmuchachos.gifmycrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifmycryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GifmycryptoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fourmuchachos/gifmycrypto/GifmycryptoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fourmuchachos.gifmycrypto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifmycryptoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
