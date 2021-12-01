6e2c55555286ed45477d2d404a5b6705c9bb2b8f
Lars <larsriel@gmail.com>
2021-12-01 18:00:57 +0100

    added randomization so the returned gif is now random

--- src/main/java/com/fourmuchachos/gifmycrypto/Gif/Service/GifServiceImpl.java
+++ src/main/java/com/fourmuchachos/gifmycrypto/Gif/Service/GifServiceImpl.java
@@ -2,13 +2,31 @@ package com.fourmuchachos.gifmycrypto.Gif.Service;
 
 import com.fourmuchachos.gifmycrypto.Gif.Entity.Gif;
 import com.fourmuchachos.gifmycrypto.Gif.Repo.GifRepo;
+import javassist.NotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class GifServiceImpl implements GifService{
-    GifRepo gifRepo;
+
+    private final GifRepo gifRepo;
+
+    public GifServiceImpl(GifRepo gifRepo) {
+        this.gifRepo = gifRepo;
+    }
+
     @Override
-    public Gif getGifByPerformance(int performance) {
-        return gifRepo.findByPerformance(performance);
+    public Gif getRandomGifByPerformance(int performance) {
+        Random r = new Random();
+
+        List<Gif> gifs = gifRepo.findAllByPerformance(performance);
+
+        if (gifs.size() == 0) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        return gifs.get(r.nextInt(gifs.size()));
     }
 }
